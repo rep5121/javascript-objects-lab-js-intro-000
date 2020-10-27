7d17a52ecc6e08d49b04d2f5568a74bbce79ca54
bold-mixer-0681 <renee.pujol@gmail.com>
2020-10-27 23:44:19 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,28 @@
+
+function updateObjectWithKeyAndValue(object, key, value){
+  var newObj = Object.assign({},object, {[key]:value})
+  return newObj
+  }
+
+
+
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value
+  return object
+  }
+
+
+
+function deleteFromObjectByKey(object, key){
+  var obj2 = Object.assign({}, object)
+  obj2
+  delete obj2.key
+  return object
+  }
+
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object[key];
+  return object
+
+  }
